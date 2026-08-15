a32aae5cd1ca58d517f2fd565b91283a65fa64c4
ahmedmohameda7222-ship-it <ahmed.mohameda7222@gmail.com>
2026-08-15 13:37:31 +0200

    Add Azkar route loading feedback

---
+++ app/azkar/loading.tsx
@@ -0,0 +1,6 @@
+import { AppShell } from "@/components/layout/AppShell";
+import { PublicRouteLoading } from "@/components/ui/PublicRouteLoading";
+
+export default function Loading() {
+  return <AppShell><PublicRouteLoading /></AppShell>;
+}
